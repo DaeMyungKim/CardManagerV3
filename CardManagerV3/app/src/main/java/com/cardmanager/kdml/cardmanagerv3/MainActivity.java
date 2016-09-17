9fcdfeb8a3fa54e3b4ea0dccd103ee5162c72fa9
Kdml <bhkdm8571@gmail.com>
2016-09-17 12:27:38 +0900

    0917

---
+++ CardManagerV3/app/src/main/java/com/cardmanager/kdml/cardmanagerv3/MainActivity.java
@@ -0,0 +1,94 @@
+package com.cardmanager.kdml.cardmanagerv3;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        checkSMSPermissions();
+    }
+
+    private void checkSMSPermissions(){
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"권한 있음",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this,"권한 없음",Toast.LENGTH_LONG).show();
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
+                Toast.makeText(this,"권한 설명 필요함.",Toast.LENGTH_LONG).show();
+            }
+            else{
+                String[] permissions = {Manifest.permission.RECEIVE_SMS};
+
+                ActivityCompat.requestPermissions(this,permissions,1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            for(int i = 0; i < permissions.length; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,permissions[i]+"권한 승인됨.",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this,permissions[i]+"권한 승인되지 않음.",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
